04c96c7abb9f12dd007595312a60ffced4062a66
Mathieu Lecarme <mlecarme@bearstech.com>
2018-09-30 16:42:51 +0200

    Draft

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
-build: bin
+build: bin vendor
 	go build -o bin/jaeger-lite .
 
 bin:
 	mkdir -p bin
+
+vendor:
+	dep ensure
+
+clean:
+	rm -rf bin vendor
--- server/server.go
+++ server/server.go
@@ -1,1 +1,21 @@
 package server
+
+import (
+	"github.com/jaegertracing/jaeger/cmd/agent/app/servers/thriftudp"
+	"github.com/uber/jaeger-lib/metrics"
+)
+
+func New() (*servers.Server, error) {
+
+	metricsFactory := metrics.NewLocalFactory(0)
+
+	transport, err := thriftudp.NewTUDPServerTransport("127.0.0.1:0")
+	if err != nil {
+		return nil, err
+	}
+	maxPacketSize := 65000
+	queueSize := 100
+	server, err := NewTBufferedServer(transport, queueSize, maxPacketSize, metricsFactory)
+
+	return server, err
+}
