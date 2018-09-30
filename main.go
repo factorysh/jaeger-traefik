4117b9b04bacc453b35c5f61e2cd1b02ec38c7fd
Mathieu Lecarme <mlecarme@bearstech.com>
2018-09-30 17:12:37 +0200

    Fix: stuff

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/factorysh/jaeger-lite/server"
+)
+
+func main() {
+	s, err := server.New()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(s)
 }
--- server/server.go
+++ server/server.go
@@ -1,21 +1,22 @@
 package server
 
 import (
+	"github.com/jaegertracing/jaeger/cmd/agent/app/servers"
 	"github.com/jaegertracing/jaeger/cmd/agent/app/servers/thriftudp"
 	"github.com/uber/jaeger-lib/metrics"
 )
 
-func New() (*servers.Server, error) {
+func New() (servers.Server, error) {
 
 	metricsFactory := metrics.NewLocalFactory(0)
 
-	transport, err := thriftudp.NewTUDPServerTransport("127.0.0.1:0")
+	transport, err := thriftudp.NewTUDPServerTransport("127.0.0.1:6831")
 	if err != nil {
 		return nil, err
 	}
 	maxPacketSize := 65000
 	queueSize := 100
-	server, err := NewTBufferedServer(transport, queueSize, maxPacketSize, metricsFactory)
+	server, err := servers.NewTBufferedServer(transport, queueSize, maxPacketSize, metricsFactory)
 
 	return server, err
 }
