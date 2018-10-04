49b40b6e098d72fa49f743de5f62835754bc60ea
Mathieu Lecarme <mlecarme@bearstech.com>
2018-10-04 16:39:16 +0200

    Oups

--- server/server.go
+++ server/server.go
@@ -1,9 +1,6 @@
 package server
 
 import (
-	"fmt"
-	"time"
-
 	"go.uber.org/zap"
 
 	"github.com/jaegertracing/jaeger/cmd/agent/app/processors"
@@ -13,94 +10,10 @@ import (
 	"github.com/jaegertracing/jaeger/cmd/agent/app/reporter"
 	"github.com/jaegertracing/jaeger/cmd/agent/app/servers/thriftudp"
 	jaegerThrift "github.com/jaegertracing/jaeger/thrift-gen/jaeger"
-	"github.com/jaegertracing/jaeger/thrift-gen/zipkincore"
-	"github.com/uber/jaeger-lib/metrics"
 )
 
 type Server interface {
 	Serve()
-}
-
-type Factory struct {
-}
-
-func (f *Factory) Counter(name string, tags map[string]string) metrics.Counter {
-	return &Counter{name, tags}
-}
-
-func (f *Factory) Timer(name string, tags map[string]string) metrics.Timer {
-	return &Timer{name, tags}
-}
-
-func (f *Factory) Gauge(name string, tags map[string]string) metrics.Gauge {
-	return &Gauge{name, tags}
-}
-
-// Namespace returns a nested metrics factory.
-func (f *Factory) Namespace(name string, tags map[string]string) metrics.Factory {
-	fmt.Println("Namespace", name, tags)
-	return f
-}
-
-type Counter struct {
-	Name string
-	Tags map[string]string
-}
-
-func (c *Counter) Inc(i int64) {
-	fmt.Println(c.Name, c.Tags, "Inc", i)
-}
-
-type Timer struct {
-	Name string
-	Tags map[string]string
-}
-
-func (t *Timer) Record(d time.Duration) {
-	fmt.Println(t.Name, t.Tags, "Record", d)
-}
-
-type Gauge struct {
-	Name string
-	Tags map[string]string
-}
-
-func (g *Gauge) Update(i int64) {
-	fmt.Println(g.Name, g.Tags, "Update", i)
-}
-
-type Reporter struct {
-}
-
-func (r *Reporter) EmitZipkinBatch(spans []*zipkincore.Span) (err error) {
-	return nil
-}
-
-func (r *Reporter) EmitBatch(batch *jaegerThrift.Batch) (err error) {
-	p := batch.GetProcess()
-	fmt.Println("batch process:", p.GetServiceName())
-	printTags(p.GetTags())
-
-	for _, span := range batch.GetSpans() {
-		printTags(span.GetTags())
-		fmt.Println("span:", span)
-	}
-	return nil
-}
-
-func printTags(tags []*jaegerThrift.Tag) {
-	for _, tag := range tags {
-		switch tag.GetVType() {
-		case jaegerThrift.TagType_STRING:
-			fmt.Println("\t", tag.GetKey(), "=>", tag.GetVStr())
-		case jaegerThrift.TagType_BOOL:
-			fmt.Println("\t", tag.GetKey(), "=>", tag.GetVBool())
-		case jaegerThrift.TagType_LONG:
-			fmt.Println("\t", tag.GetKey(), "=>", tag.GetVLong())
-		default:
-			fmt.Println("\t", tag.GetKey(), tag.GetVType(), tag)
-		}
-	}
 }
 
 func New() (Server, error) {
