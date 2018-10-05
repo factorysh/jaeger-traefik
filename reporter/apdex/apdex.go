1704e0b1392853d59c7409bf8f1c01405f9e7e54
Mathieu Lecarme <mlecarme@bearstech.com>
2018-10-05 18:23:30 +0200

    More APDEX

--- reporter/apdex/apdex.go
+++ reporter/apdex/apdex.go
@@ -1,13 +1,23 @@
 package apdex
 
 import (
+	"time"
+
 	jaegerThrift "github.com/jaegertracing/jaeger/thrift-gen/jaeger"
 	"github.com/jaegertracing/jaeger/thrift-gen/zipkincore"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+const (
+	Satisfied   = "satisified"
+	Tolerating  = "toleration"
+	Unsatisfied = "unsatisfied"
+)
+
 type ApdexReporter struct {
-	gauge prometheus.Gauge
+	SatisfiedTarget  time.Duration
+	ToleratingTarget time.Duration
+	CounterVec       *prometheus.CounterVec
 }
 
 func (a *ApdexReporter) EmitZipkinBatch(spans []*zipkincore.Span) (err error) {
@@ -15,14 +25,104 @@ func (a *ApdexReporter) EmitZipkinBatch(spans []*zipkincore.Span) (err error) {
 }
 
 func (a *ApdexReporter) EmitBatch(batch *jaegerThrift.Batch) (err error) {
+	/*
+		process:
+		   batch process: traefik
+		   	 jaeger.version => Go-2.9.0
+		   	 hostname => c626598ba941
+		   	 ip => 172.29.0.4
+		   	 span.kind => client
+		   	 frontend.name => frontend-Host-traefik-2
+		   	 backend.name => backend-web-demo
+		   	 http.method => GET
+		   	 http.url => http://172.29.0.2:80/
+		   	 http.host => traefik
+		   	 http.status_code => 200
+
+		span:
+			sampler.type => const
+			sampler.param => true
+			span.kind => server
+			component => traefik
+			http.method => GET
+			http.url => /
+			http.host => traefik
+			span.kind => server
+			http.status_code => 200
+	*/
+	p := batch.GetProcess()
+	if p.GetServiceName() != "traefik" {
+		return nil
+	}
+
+	var backend, host string
+	for _, tag := range p.GetTags() {
+		switch tag.GetKey() {
+		case "backend.name":
+			backend = tag.GetVStr()
+		case "http.host":
+			host = tag.GetVStr()
+		}
+	}
+	for _, span := range batch.GetSpans() {
+		var satisfaction string
+		for _, tag := range span.GetTags() {
+			switch tag.GetKey() {
+			case "span.kind":
+				if tag.GetVStr() != "server" {
+					return nil
+				}
+			case "component":
+				if tag.GetVStr() != "traefik" {
+					return nil
+				}
+			case "http.status_code":
+				status := tag.GetVLong()
+				if status < 200 { // 1xx
+					return nil
+				}
+				if status >= 300 && status < 500 { // 3xx, 4xx
+					return nil
+				}
+				if status >= 500 {
+					satisfaction = Unsatisfied
+				}
+			}
+		}
+		if satisfaction == "" {
+			d := time.Duration(span.GetDuration()) * time.Millisecond
+			if d <= a.SatisfiedTarget {
+				satisfaction = Satisfied
+			} else if d <= a.ToleratingTarget {
+				satisfaction = Tolerating
+			} else {
+				satisfaction = Unsatisfied
+			}
+		}
+		a.CounterVec.With(prometheus.Labels{
+			"satisfaction": satisfaction,
+			"backend":      backend,
+			"host":         host,
+		}).Inc()
+	}
+
 	return nil
 }
 
-func New() *ApdexReporter {
-	return &ApdexReporter{
-		gauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "apdex",
-			Help: "Apdex compute from http status and performance, per domain",
-		}),
+func New(toleratingTarget, satisfiedTarget time.Duration) *ApdexReporter {
+	a := &ApdexReporter{
+		ToleratingTarget: toleratingTarget,
+		SatisfiedTarget:  satisfiedTarget,
+		CounterVec: prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "apdex",
+				Help: "APDEX raw data, clustered by satisfaction",
+			},
+			[]string{"satisfaction", "domain", "backend"},
+		),
 	}
+
+	prometheus.MustRegister(a.CounterVec)
+
+	return a
 }
