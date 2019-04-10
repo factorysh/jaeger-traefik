fc2a3a270e416f8b2ed510ad01cbe6384a435445
Mathieu Lecarme <mlecarme@bearstech.com>
2019-04-10 22:46:36 +0200

    More tags.

--- reporter/traefik.go
+++ reporter/traefik.go
@@ -4,11 +4,15 @@ import (
 	"time"
 
 	"github.com/jaegertracing/jaeger/thrift-gen/jaeger"
+	log "github.com/sirupsen/logrus"
 )
 
 type Traefik struct {
 	Backend    string        // backend.name
+	Frontend   string        // frontend.name
+	URL        string        // http.url
 	Host       string        // http.host
+	Method     string        // http.method
 	StatusCode int64         // http.status_code
 	Duration   time.Duration // duration
 }
@@ -16,6 +20,7 @@ type Traefik struct {
 func TraefikSpan(span *jaeger.Span) *Traefik {
 	t := &Traefik{}
 	for _, tag := range span.GetTags() {
+		log.WithField(tag.GetKey(), tag).Debug("TraefikSpan")
 		switch tag.GetKey() {
 		case "span.kind":
 			if tag.GetVStr() != "server" {
@@ -23,8 +28,14 @@ func TraefikSpan(span *jaeger.Span) *Traefik {
 			}
 		case "backend.name":
 			t.Backend = tag.GetVStr()
+		case "frontend.name":
+			t.Frontend = tag.GetVStr()
 		case "http.host":
 			t.Host = tag.GetVStr()
+		case "http.url":
+			t.URL = tag.GetVStr()
+		case "http.method":
+			t.Method = tag.GetVStr()
 		case "component":
 			if tag.GetVStr() != "traefik" {
 				return nil
